7ed64f13f30d711161306843fb5cc23178ce489c
Aswanth-k-0 <144346822+Aswanth-k-0@users.noreply.github.com>
2024-02-08 10:41:10 +0530

    KK

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ app.get('/notifications',(req,res)=>{
 
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // Specify the directory where uploaded files should be stored
+      cb(null, '../frontend/src/pages/uploads/'); // Specify the directory where uploaded files should be stored
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname); // Use the original file name for storing
